5c6f31b9ab7012eb276f579059e32dbf947bb524
Jaspreet Kaur <reader.jaspreet@gmail.com>
2024-03-01 10:20:23 +0530

    node commit and assignment-2 second commit

--- Assignment-2/squareNumber.js
+++ Assignment-2/squareNumber.js
@@ -1,3 +1,9 @@
-const prompt=require('prompt-sync')();
-const num=parseInt(prompt("Enter a number"));
+const prompt=require("prompt-sync")();
+const number=parseInt(prompt("Enter a number"));
 
+let square;
+const squareNumber=()=>{
+    square=number*number;
+    console.log(`the square of {number} is : ${square}`);
+}
+squareNumber();
