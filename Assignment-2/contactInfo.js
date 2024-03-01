2eac4ae85dcd290829b2598e54814e2528dadd64
Jaspreet Kaur <reader.jaspreet@gmail.com>
2024-03-01 19:27:13 +0530

    contactinfo commit

---
+++ Assignment-2/contactInfo.js
@@ -0,0 +1,22 @@
+const contactInfo=new Map();
+
+//Adding contacts to maps
+
+contactInfo.set("John",{
+    age:23,
+    email:"john_cool@gmail.com",
+    location:"california",
+});
+contactInfo.set("Jennifer",{
+    age:33,
+    email:"jennifer2015@yahoo.co.in",
+    location:"texas",
+});
+contactInfo.set("Amanda",{
+    age:26,
+    email:"reader.amanda@yahoo.co.in",
+    location:"boston",
+});
+
+const getContact=(name)=> {return contactInfo.get(name);}
+console.log(getContact("Jennifer"));
