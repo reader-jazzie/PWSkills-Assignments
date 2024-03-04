c9fc455e238db562451e5afc326c68ae40a24ffe
Jaspreet Kaur <reader.jaspreet@gmail.com>
2024-03-05 00:34:05 +0530

    modify_callfunc.js

--- Assignment-2/callfunc.js
+++ Assignment-2/callfunc.js
@@ -1,16 +1,10 @@
 person1={
     name:"Jaspreet Kaur",
-    
-     introduce(age){
-        console.log(`Hello , I am ${this.name} and I am ${this.age} years old `)
-     }
+    age:35,
+     introduce(){console.log(`Hello ,I am ${this.name} and I am ${this.age} years old `)}
 }
 
-person2={
-    name:"Jasgun Kaur",
-    age:6
-    
-}
+person2={name:"Jasgun Kaur",age:6}
 
-let result=person1.introduce.call(person2)
-console.log(result);
+let result=person1.introduce.call(person2);
+
